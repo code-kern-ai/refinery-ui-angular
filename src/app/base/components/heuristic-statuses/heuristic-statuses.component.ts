6cba9f1600c79a2325145f91fad3dceb3872bf03
Lina <lina.lumburovska@kern.ai>
2022-08-03 14:28:30 +0200

    Refactor heuristic statuses component

--- src/app/base/components/heuristic-statuses/heuristic-statuses.component.ts
+++ src/app/base/components/heuristic-statuses/heuristic-statuses.component.ts
@@ -10,9 +10,34 @@ export class HeuristicStatusesComponent implements OnInit {
   @Input() status: string;
   @Input() tooltipPosition: string = 'tooltip-bottom';
 
+  dataTip: string;
+  statusName: string;
+  color: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    switch(this.status) {
+      case 'CREATED':
+        this.dataTip = 'Heuristic is currently being executed.';
+        this.statusName = 'Running';
+        this.color = 'yellow';
+        break;
+      case 'FINISHED':
+        this.dataTip = 'Heuristic was successfully executed.';
+        this.statusName = 'Finished';
+        this.color = 'green';
+        break;
+      case 'FAILED':
+        this.dataTip = 'Heuristic ran into errors.';
+        this.statusName = 'Error';
+        this.color = 'red';
+        break;
+      default: 
+        this.dataTip = 'Heuristic was successfully registered.';
+        this.statusName = 'Initial';
+        this.color = 'gray';
+    }
   }
 
 }
