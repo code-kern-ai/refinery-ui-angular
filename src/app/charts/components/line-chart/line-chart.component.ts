0cf74636f728f30c83ebbe79e894d8ffa0de602f
Lina <lina.lumburovska@kern.ai>
2022-09-28 16:01:53 +0200

    Added grid on confidence distribution matrix

--- src/app/charts/components/line-chart/line-chart.component.ts
+++ src/app/charts/components/line-chart/line-chart.component.ts
@@ -63,6 +63,34 @@ export class LineChartComponent implements OnInit, OnChanges {
       .domain([0, d3.max(data, function (d) { return +d.value; })])
       .range([height, 0]);
 
+    var g = svg.append("g").attr("transform", "translate(0,0)");
+      g.append("g")
+      .attr("class", "y axis")
+      .style("font-size", 14)
+      .style('font-family', '"DM Sans", sans-serif')
+      // setting up y axis opacity to 0 before transition
+      .style("opacity", "0")
+      .call(d3.axisLeft(y).ticks(null, "s").tickFormat(d3.format(",.0%"))).selectAll(".tick").each(function (data) {
+        var tick = d3.select(this);
+        // pull the transform data out of the tick
+        var positionString = tick.attr("transform")
+        var position = parseFloat(positionString.substring(12, positionString.length - 1))
+
+        g.append('line')
+          .style("stroke", "lightgray")
+          .style("stroke-width", 1)
+          .attr("x1", 0)
+          .attr("y1", position)
+          .attr("x2", width)
+          .attr("y2", position)
+          .style("stroke-opacity", "0")
+          .transition()
+          .duration(500)
+          .delay(600)
+          // setting up full opacity after transition
+          .style("stroke-opacity", "0.5");
+      });
+
 
     // Add the area
     svg.append("path")
