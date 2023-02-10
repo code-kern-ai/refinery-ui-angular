adcafc13592bbead37535bd33fe58c821a5a0716
JWittmeyer <jens.wittmeyer@onetask.ai>
2023-02-10 13:37:06 +0100

    Adds fix & changes the api position

--- src/app/data/components/data-browser/helper-classes/filter-parser.ts
+++ src/app/data/components/data-browser/helper-classes/filter-parser.ts
@@ -335,13 +335,16 @@ export class DataBrowserFilterParser {
             for (let i = 1; i < this.dataBrowser.attributesSortOrder.length; i++) {
                 searchElement.values.operator = searchElement.values.operator.split(" ").join("_");
                 if (this.dataBrowser.attributesSortOrder[i].type != 'BOOLEAN') {
+                    const filterValues = prepareFilterElements(searchElement, this.dataBrowser.attributes[this.dataBrowser.attributesSortOrder[i].key].name, this.dataBrowser.dataBrowserModals.configuration.separator, this.dataBrowser.attributesSortOrder[i].type);
+                    console.log(filterValues)
+                    if (!filterValues) continue;
                     filterElement.FILTER.push({
                         RELATION: i == 1 ? 'NONE' : 'OR',
                         NEGATION: false,
                         TARGET_TABLE: 'RECORD',
                         TARGET_COLUMN: 'DATA',
                         OPERATOR: prepareOperator(searchElement, this.dataBrowser.attributesSortOrder[i].type),
-                        VALUES: prepareFilterElements(searchElement, this.dataBrowser.attributes[this.dataBrowser.attributesSortOrder[i].key].name, this.dataBrowser.dataBrowserModals.configuration.separator, this.dataBrowser.attributesSortOrder[i].type),
+                        VALUES: filterValues,
                     });
                 }
             }
