5040958d53c27db24deeadd15699f040f93e9882
JWittmeyer <jens.wittmeyer@kern.ai>
2023-04-04 15:48:59 +0200

    hotfix

--- src/app/base/components/bricks-integrator/helper/dummy-nodes.ts
+++ src/app/base/components/bricks-integrator/helper/dummy-nodes.ts
@@ -105,7 +105,8 @@ export function getAddInfo(type: BricksVariableType, asEnum: boolean): string[] 
         case BricksVariableType.LABEL:
         case BricksVariableType.LABELING_TASK:
         case BricksVariableType.LOOKUP_LIST:
-            list.push("BricksVariableType.GENERIC_STRING.value");
+            if (asEnum) list.push("BricksVariableType.GENERIC_STRING.value");
+            else list.push(BricksVariableType.GENERIC_STRING.toLowerCase());
             break;
     }
 
