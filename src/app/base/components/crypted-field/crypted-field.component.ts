e1b9a6526d0067ab81b96f1215d9986fe364ecae
Lina <lina.lumburovska@kern.ai>
2023-06-21 09:55:29 +0200

    Code improvements

--- src/app/base/components/crypted-field/crypted-field.component.ts
+++ src/app/base/components/crypted-field/crypted-field.component.ts
@@ -1,19 +1,16 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'kern-crypted-field',
   templateUrl: './crypted-field.component.html',
   styleUrls: ['./crypted-field.component.scss']
 })
-export class CryptedFieldComponent implements OnInit {
+export class CryptedFieldComponent {
   @Output() keyChange = new EventEmitter<string>();
   key: string = '';
   show: boolean = false;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   setKey(key: string) {
     this.key = key;
