80626920c50b161879e115138c6e6bf6f56d3a0b
JWittmeyer <jens.wittmeyer@kern.ai>
2023-06-29 19:37:55 +0200

    Password change

--- src/app/base/components/crypted-field/crypted-field.component.ts
+++ src/app/base/components/crypted-field/crypted-field.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, EventEmitter, Input, OnDestroy, Output, ViewChild } from '@angular/core';
 
-const HIDDEN = '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
 
 @Component({
   selector: 'kern-crypted-field',
@@ -14,9 +13,7 @@ export class CryptedFieldComponent implements OnDestroy {
 
   @ViewChild('inputElement') inputElement: ElementRef;
   key: string = '';
-  hiddenKey: string = '';
   show: boolean = false;
-  saveKey: string = '';
 
   constructor() { }
 
@@ -26,35 +23,11 @@ export class CryptedFieldComponent implements OnDestroy {
   }
 
   setKey(key: any) {
-    this.saveKey += key[key.length - 1];
     this.key = key;
-    this.hiddenKey = HIDDEN.substring(0, this.key.length);
-    this.keyChange.emit(this.saveKey);
+    this.keyChange.emit(this.key);
   }
 
   toggleKey() {
     this.show = !this.show;
-  }
-
-
-  onBackspace(event) {
-    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-      return;
-    }
-    if (this.inputElement.nativeElement.selectionStart === 0 && !(event.ctrlKey || event.metaKey)) {
-      this.inputElement.nativeElement.value = '';
-      this.key = '';
-      this.saveKey = '';
-      this.hiddenKey = '';
-      this.keyChange.emit(this.saveKey);
-      return;
-    }
-    if (event.key === 'Backspace') {
-      event.preventDefault();
-      this.saveKey = this.saveKey.substring(0, this.saveKey.length - 1);
-      this.key = this.key.substring(0, this.key.length - 1);
-      this.hiddenKey = HIDDEN.substring(0, this.key.length);
-      this.keyChange.emit(this.saveKey);
-    }
   }
 }
